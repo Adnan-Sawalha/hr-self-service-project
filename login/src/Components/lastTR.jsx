ff615608090cfaf74ee7f595e7947adc2134de13
Adnan-Sawalha <adnan.sawalha04@gmail.com>
2024-07-24 17:32:57 +0300

    added a popup when you click plus

--- login/src/Components/lastTR.jsx
+++ login/src/Components/lastTR.jsx
@@ -1,4 +1,6 @@
 import styles from "../MainPage/MainPage.module.css";
+import Popup from "reactjs-popup";
+import "reactjs-popup/dist/index.css";
 
 function lastTR() {
   return (
@@ -8,7 +10,16 @@ function lastTR() {
       </td>
       <td className={styles.tableCont}>
         <div>
-          <button type="button">+</button>
+          <Popup trigger={<button>+</button>} modal nested>
+            {(close) => (
+              <div className="modal">
+                <div className="content">Welcome to GFG!!!</div>
+                <div>
+                  <button onClick={() => close()}>Close modal</button>
+                </div>
+              </div>
+            )}
+          </Popup>
         </div>
       </td>
     </tr>
